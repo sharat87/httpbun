726045c0762869c1e3d87e2e014fb8bb412682ba
Alex Li <github@alexv525.com>
2023-12-02 20:51:03 +0800

    ✅ Add status tests

---
+++ api-tests/status_test.go
@@ -0,0 +1,17 @@
+package api_tests
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStatus(t *testing.T) {
+	s := assert.New(t)
+	for i := 100; i <= 599; i++ {
+		resp, _ := ExecRequest(R{
+			Path: fmt.Sprintf("status/%d", i),
+		})
+		s.Equal(i, resp.StatusCode)
+	}
+}
